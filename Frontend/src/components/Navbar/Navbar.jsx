21db485ec6890485784d96686873e1214e22faac
anushome09 <anushome081101@gmail.com>
2024-07-10 20:37:25 +0530

    Created navigation component

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 // using the CSS file of the navbar component
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 // mount the navbar in the app component
 
 const Navbar = () => {
+
+  // this state variable are created to add the effect of underline in the links
+  const [menu, setMenu] = useState("home")
+
   return (
     <div className='navbar'>
-      
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
