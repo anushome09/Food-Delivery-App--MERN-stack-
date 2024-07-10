c5f8f3a6ecb620dacb18bf12427f81ee762ea92c
anushome09 <anushome081101@gmail.com>
2024-07-10 20:04:17 +0530

    Updated the Assests folder

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// using the CSS file of the navbar component
+import './Navbar.css'
+// mount the navbar in the app component
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
+
+export default navbar
